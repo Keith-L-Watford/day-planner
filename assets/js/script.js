0514c73acddb8401d2eb80e07687cb45a6a6e450
Keith Watford <keith.l.watford@gmail.com>
2021-06-11 10:07:10 -0400

    Starting to figure out the color time logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,6 @@ var time = moment().format('[ and the time is] hh:mm a')
 $(".current-day").text(today + time);
 
 
-// var currentTime = moment().format("[It it currently] hh:mm a")
-// $("#current-time").text(today);
-
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
@@ -22,21 +19,29 @@ $(".current-day").text(today + time);
 // relative time rounding ?? (math floor)
 // or maybe its more of a switch statement, where when hour = moment().format(hh) 
 // and when each hour equals the same number (id?) in the box it will change to green.if less than number, turns red, if greater than it turns gray or whatever
+var currentHour = moment().format('hh')
+var nineHour = document.getElementById("input-9am").value
+
+console.log(currentHour);
+console.log(nineHour);
+
+// function whatTimeIsIt() {
+    
+// }
+
+// if (currentHour < nineHour) {
+//     document.element.classList.add("future");
+// } else if (currentHour > nineHour) {
+//     document.element.classList.add("past");
+// } else {
+//     document.element.classList.add("present")
+// }
 
 
 // WHEN*** I click into a timeblock
 // THEN*** I can enter an event
-
-
 // WHEN***** I click the save button for that timeblock
 // THEN***** the text for that event is saved in local storage
-// function init() {
-//     var lastEntryNine = JSON.parse(localStorage.getItem("keyNine"));
-
-//     if (lastEntryNine == null) {
-//         userInputNine = lastEntryNine;
-//     }
-// }
 
 // 9am feature
 var userInputNine = document.getElementById("input-9am")
@@ -48,13 +53,28 @@ saveButtonNine.addEventListener("click", function (event) {
 
     localStorage.setItem("keyNine", JSON.stringify(userInputNine));
     console.log(userInputNine);
-
 });
-
 function renderMessageNine() {
     var lastEntryNine = JSON.parse(localStorage.getItem("keyNine"));
     if (lastEntryNine !== null) {
         document.getElementById('input-9am').value = lastEntryNine
+    }
+}
+
+// 10am feature
+var userInputTen = document.getElementById("input-10am")
+var saveButtonTen = document.getElementById("save-button-10am");
+saveButtonTen.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var userInputTen = document.getElementById('input-10am').value.trim()
+
+    localStorage.setItem("keyTen", JSON.stringify(userInputTen));
+});
+function renderMessageTen() {
+    var lastEntryTen = JSON.parse(localStorage.getItem("keyTen"));
+    if (lastEntryTen !== null) {
+        document.getElementById('input-10am').value = lastEntryTen
     }
 }
 
@@ -63,10 +83,10 @@ function renderMessageNine() {
 // THEN the saved events persist
 renderMessageNine();
 renderMessageTen();
-renderMessageEleven();
-renderMessageNoon();
-renderMessageOne();
-renderMessageTwo();
-renderMessageThree();
-renderMessageFour();
-renderMessageFive();
+// renderMessageEleven();
+// renderMessageNoon();
+// renderMessageOne();
+// renderMessageTwo();
+// renderMessageThree();
+// renderMessageFour();
+// renderMessageFive();
